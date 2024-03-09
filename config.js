78825529bca631056b6dc072ace845b3798dd25d
vxdiep168 <161051125+vxdiep168@users.noreply.github.com>
2024-03-09 16:42:00 +0700

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ const nameGirl = 'Ngọc Linh';
 const giftUrl = 'https://www.facebook.com/profile.php?id=100054847509209';
 const eventName = 'Chúc mừng 8-3';
 const titleCard = 'Tặng Bắp';
-const contentCard = 'Nhân ngày Quốc tế Phụ nữ 8/3 tớ xin chúc Bắp của tớ luôn cute, đáng yêu, vui tươi, xinh đẹp và đặc biệt là khoẻ mạnh để có được tâm thế tốt nhất trong quá trình chuẩn bị cho kỳ thi quyết định của cuộc đời nha. Thêm một điều quan trọng nữa là chúc Bắp đạt được điểm cao vượt mong đợi trong kỳ thi và lựa chọn nguyện vọng của mình kỹ càng nha :3';
+const contentCard = 'Nhân ngày Quốc tế Phụ nữ 8/3 tớ xin chúc Bắp của tớ luôn cute, đáng yêu, vui tươi, xinh đẹp và đặc biệt là khoẻ mạnh để có được tâm thế tốt nhất trong quá trình chuẩn bị cho kỳ thi quyết định của cuộc đời nha. Thêm một điều quan trọng nữa là chúc Bắp đạt được điểm cao vượt mong đợi trong kỳ thi và lựa chọn nguyện vọng của mình kỹ càng nha 🩷';
 const giftImage = 'hot-girl.png';
 const base64 = '';
 const giftImageBase64 = "data:image/png;base64, " + base64;
